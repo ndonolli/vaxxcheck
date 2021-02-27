b76d1525d08104378366db83bbc5b744537fbb59
Nathan Donolli <ndonolli@aligntoday.com>
2021-02-26 23:43:30 -0600

    fix redundancy in formatdate

--- vaxcheck.js
+++ vaxcheck.js
@@ -1,8 +1,7 @@
 let coords = {};
 const url = 'https://www.walgreens.com/hcschedulersvc/svc/v1/immunizationLocations/availability';
 
-const formatDate = date => {
-  const d = new Date(date);
+const formatDate = d => {
   let month = '' + (d.getMonth() + 1);
   let day = '' + (d.getDate() + 1);
   let year = d.getFullYear();
@@ -101,6 +100,6 @@ const submit = () => {
         }
       });
     }
-  });
+  }, err => alert(err.message));
   
 }
